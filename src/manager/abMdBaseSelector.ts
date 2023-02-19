fee54989a4e705de38e6e28a01875f43a75f2989
LincZero <762699299@qq.com>
2023-02-19 21:04:59 +0800

    re add: head-tail selector

--- src/manager/abMdBaseSelector.ts
+++ src/manager/abMdBaseSelector.ts
@@ -5,137 +5,116 @@ import {
   type MdSelectorRangeSpecSimp
 } from "./abMdSelector"
 
-const mdSelector_brace:MdSelectorSpec = {
-  match: ABReg.reg_front,
+function easySelector(
+  list_text:string[],
+  from_line:number,
+  selector:string, 
+  frist_reg:RegExp
+):MdSelectorRangeSpecSimp|null{
+  let mdRange:MdSelectorRangeSpecSimp = {
+    from_line: from_line-1,
+    to_line: from_line+1,
+    header: "",
+    selector: selector,
+    content: "",
+    prefix: ""
+  }
+  // 验证首行
+  if (from_line <= 0) return null
+  const first_line_match = list_text[from_line].match(frist_reg)
+  if (!first_line_match) return null
+  mdRange.prefix = first_line_match[1]  // 可以是空
+  // 验证header
+  let header_line_match:RegExpMatchArray | null
+  if (ABReg.reg_emptyline.test(list_text[from_line-1]) && from_line>1){
+    from_line = from_line-2
+  }
+  header_line_match = list_text[mdRange.from_line].match(ABReg.reg_header)
+  if (!header_line_match
+    || !header_line_match[4] 
+    || header_line_match[1]!=mdRange.prefix
+  ) return null
+  mdRange.header = header_line_match[4]
+  return mdRange
+}
+
+function easySelector_headtail(
+  list_text:string[],
+  from_line:number,
+  selector:string, 
+  frist_reg:RegExp
+):MdSelectorRangeSpecSimp|null{
+  let mdRange:MdSelectorRangeSpecSimp = {
+    from_line: from_line,
+    to_line: from_line+1,
+    header: "",
+    selector: selector,
+    content: "",
+    prefix: ""
+  }
+  // 验证首行
+  if (from_line <= 0) return null
+  const first_line_match = list_text[from_line].match(frist_reg)
+  if (!first_line_match) return null
+  mdRange.prefix = first_line_match[1]  // 可以是空
+  // 验证header
+  mdRange.header = first_line_match[4]
+  return mdRange
+}
+
+const mdSelector_headtail:MdSelectorSpec = {
+  match: ABReg.reg_headtail,
   selector: (list_text, from_line)=>{
-    let mdRange:MdSelectorRangeSpecSimp = {
-        from_line: from_line-1,
-        to_line: from_line+1,
-        header: "",
-        selector: "barce",
-        content: "",
-        prefix: ""
-    }
-    // 验证首行
-    if (from_line <= 0) return null
-    const first_line_match = list_text[from_line].match(ABReg.reg_front)
-    if (!first_line_match) return null
-    mdRange.prefix = first_line_match[1]  // 可以是空
-    // 验证header
-    const header_line_match = list_text[from_line-1].match(ABReg.reg_header)
-    if (!header_line_match) return null
-    if (!header_line_match
-      || !header_line_match[3] 
-      || header_line_match[1]!=mdRange.prefix
-    ) return null
-    mdRange.header = header_line_match[3]
+    let mdRangeTmp = easySelector_headtail(list_text, from_line, "headtail", ABReg.reg_headtail)
+    if (!mdRangeTmp) return null
+    const mdRange = mdRangeTmp
     // 开头找到了，现在开始找结束。不需要循环尾处理器
     let last_nonempty:number = from_line
     for (let i=from_line+1; i<list_text.length; i++){
       const line = list_text[i]
+      // 前缀不符合
       if (line.indexOf(mdRange.prefix)!=0) break
       const line2 = line.replace(mdRange.prefix, "")    // 删掉无用前缀
-      if (ABReg.reg_end.test(line2)) {         // 列表
-        last_nonempty = i
-        continue 
-      }
-      if (/^\s+?\S/.test(line2)) {              // 开头有缩进
-        last_nonempty = i
-        continue
-      }
-      if (/^\s*$/.test(line2)) {                // 空行
-        last_nonempty = i
-        continue
-      }
-      break
+      // 空行
+      if (ABReg.reg_emptyline.test(line2)) {continue}
+      last_nonempty = i
+      // 结束
+      if (ABReg.reg_headtail.test(line2)) {last_nonempty = i; break}
     }
     mdRange.to_line = last_nonempty+1
     mdRange.content = list_text
-      .slice(mdRange.from_line+1, mdRange.to_line)
+      .slice(mdRange.from_line+1, mdRange.to_line-1)
       .map((line)=>{return line.replace(mdRange.prefix, "")})
       .join("\n")
     return mdRange
   }
 }
-registerMdSelector(mdSelector_brace)
-
-/*const mdSelector_brace:MdSelectorSpec = {
-  match: ABReg.reg_front,
-  selector: (mdText, from, confSelect)=>{
-    const matchInfo: MdSelectorRangeSpec[] = []
-    const list_text = this.mdText.split("\n")
-    let prev_front_line:number[] = []
-    for (let i=0; i<list_text.length; i++){
-      if(ABReg.reg_front.test(list_text[i])){       // 前缀
-        prev_front_line.push(i)
-      }
-      else if(ABReg.reg_end.test(list_text[i])){    // 后缀
-        if(prev_front_line && prev_front_line.length>0){
-          const from_line = prev_front_line.pop()??0 // @warning 有可能pop出来undefine?
-          const from = this.map_line_ch[from_line]
-          const to = this.map_line_ch[i+1]-1
-          matchInfo.push({
-            from: from,
-            to: to,
-            header: list_text[from_line].slice(2,-1),
-            selector: "brace",
-            content: this.mdText.slice(this.map_line_ch[from_line+1], to-3)
-          })
-        }
-      }
-    }
-    return matchInfo
-  }
-}
-registerMdSelector(mdSelector_brace)*/
+registerMdSelector(mdSelector_headtail)
 
 const mdSelector_list:MdSelectorSpec = {
   match: ABReg.reg_list,
   selector: (list_text, from_line)=>{
-    let mdRange:MdSelectorRangeSpecSimp = {
-        from_line: from_line-1,
-        to_line: from_line+1,
-        header: "",
-        selector: "list",
-        content: "",
-        prefix: ""
-    }
-    // 验证首行
-    if (from_line <= 0) return null
-    const first_line_match = list_text[from_line].match(ABReg.reg_list)
-    if (!first_line_match) return null
-    mdRange.prefix = first_line_match[1]  // 可以是空
-    // 验证header
-    const header_line_match = list_text[from_line-1].match(ABReg.reg_header)
-    if (!header_line_match) return null
-    if (!header_line_match
-      || !header_line_match[3] 
-      || header_line_match[1]!=mdRange.prefix
-    ) return null
-    mdRange.header = header_line_match[3]
+    let mdRangeTmp = easySelector(list_text, from_line, "list", ABReg.reg_list)
+    if (!mdRangeTmp) return null
+    const mdRange = mdRangeTmp
     // 开头找到了，现在开始找结束。不需要循环尾处理器
     let last_nonempty:number = from_line
     for (let i=from_line+1; i<list_text.length; i++){
       const line = list_text[i]
+      // 前缀不符合
       if (line.indexOf(mdRange.prefix)!=0) break
       const line2 = line.replace(mdRange.prefix, "")    // 删掉无用前缀
-      if (ABReg.reg_list.test(line2)) {         // 列表
-        last_nonempty = i
-        continue 
-      }
-      if (/^\s+?\S/.test(line2)) {              // 开头有缩进
-        last_nonempty = i
-        continue
-      }
-      if (/^\s*$/.test(line2)) {                // 空行
-        last_nonempty = i
-        continue
-      }
+      // 列表
+      if (ABReg.reg_list.test(line2)) {last_nonempty = i; continue}
+      // 开头有缩进
+      if (ABReg.reg_indentline.test(line2)) {last_nonempty = i; continue}
+      // 空行
+      if (ABReg.reg_emptyline.test(line2)) {continue}
       break
     }
     mdRange.to_line = last_nonempty+1
     mdRange.content = list_text
-      .slice(mdRange.from_line+1, mdRange.to_line)
+      .slice(mdRange.from_line, mdRange.to_line)
       .map((line)=>{return line.replace(mdRange.prefix, "")})
       .join("\n")
     return mdRange
