8fa4f819ec988a814dc007d12f512fd0023c557a
Linc <762699299@qq.com>
2024-08-07 14:57:04 +0800

    add: (abc) abc_list2pumlMindmap

--- src/ABConverter/converter/abc_plantuml.ts
+++ src/ABConverter/converter/abc_plantuml.ts
@@ -7,6 +7,34 @@
 
 import {ABConvert_IOEnum, ABConvert, type ABConvert_SpecSimp} from "./ABConvert"
 import {ABConvertManager} from "../ABConvertManager"
+import {ListProcess, type List_ListItem} from "./abc_list"
 
+const abc_list2jsontext = ABConvert.factory({
+  id: "list2jsontext",
+  name: "列表到json文本",
+  process_param: ABConvert_IOEnum.text,
+  process_return: ABConvert_IOEnum.text,
+  process: (el, header, content)=>{
+    content = "Developing..."
+    return content
+  }
+})
 
+const abc_list2pumlMindmap = ABConvert.factory({
+  id: "list2pumlMindmap",
+  name: "列表到puml思维导图",
+  process_param: ABConvert_IOEnum.text,
+  process_return: ABConvert_IOEnum.el,
+  process: (el, header, content)=>{
+    const listdata:List_ListItem = ListProcess.list2data(content)
 
+    let newContent = "```plantuml\n@startmindmap\n"
+    for (let item of listdata) {
+      newContent += "*".repeat(item.level+1) + " " + item.content + "\n"
+    }
+    newContent += "@endmindmap\n```"
+
+    ABConvertManager.getInstance().m_renderMarkdownFn(newContent, el)
+    return el
+  }
+})
