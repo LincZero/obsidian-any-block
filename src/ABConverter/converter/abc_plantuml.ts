84dedb5c800a7976f8429194ed82bd9e449eaf3f
Linc <762699299@qq.com>
2024-08-08 14:38:02 +0800

    add: (abc_plantuml) new method to use plantuml

--- src/ABConverter/converter/abc_plantuml.ts
+++ src/ABConverter/converter/abc_plantuml.ts
@@ -9,6 +9,8 @@ import {ABConvert_IOEnum, ABConvert, type ABConvert_SpecSimp} from "./ABConvert"
 import {ABConvertManager} from "../ABConvertManager"
 import {ListProcess, type List_ListItem} from "./abc_list"
 
+import plantumlEncoder from "plantuml-encoder"
+
 const abc_list2jsontext = ABConvert.factory({
   id: "list2jsontext",
   name: "列表到json文本",
@@ -27,14 +29,32 @@ const abc_list2pumlMindmap = ABConvert.factory({
   process_return: ABConvert_IOEnum.el,
   process: (el, header, content)=>{
     const listdata:List_ListItem = ListProcess.list2data(content)
-
-    let newContent = "```plantuml\n@startmindmap\n"
+    let newContent = "@startmindmap\n"
     for (let item of listdata) {
       newContent += "*".repeat(item.level+1) + " " + item.content + "\n"
     }
-    newContent += "@endmindmap\n```"
+    newContent += "@endmindmap"
 
-    ABConvertManager.getInstance().m_renderMarkdownFn(newContent, el)
+    render_pumlText(newContent, el)
     return el
   }
 })
+
+async function render_pumlText(text: string, div: HTMLElement) {
+    // 1. 四选一。自己渲 (优缺点见abc_mermaid的相似方法)
+    var encoded = plantumlEncoder.encode(text)
+    let url = 'http://www.plantuml.com/plantuml/img/' + encoded
+    div.innerHTML = `<img src="${url}">`
+
+    // 2. 四选一。这里给环境渲染 (优缺点见abc_mermaid的相似方法)
+    //ABConvertManager.getInstance().m_renderMarkdownFn("```plantuml\n"+text+"```", div)
+
+    // 3. 四选一。这里不渲，交给上一层让上一层渲 (优缺点见abc_mermaid的相似方法)
+    //div.classList.add("ab-raw")
+    //div.innerHTML = `<div class="ab-raw-data" type-data="plantuml" content-data='${text}'></div>`
+
+    // 4. 四选一。纯动态/手动渲染 (优缺点见abc_mermaid的相似方法)
+    // ...
+
+    return div
+}
